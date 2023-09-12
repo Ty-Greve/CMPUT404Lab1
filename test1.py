f9ea51d274d0e47e5f21206633a350114fa7bd51
Tyga <tyga_ii@Tygas-MacBook-Pro-2.local>
2023-09-11 18:42:57 -0600

    script downloads itself

--- test1.py
+++ test1.py
@@ -2,6 +2,6 @@ import requests
 
 print(requests.__version__)
 
-url = "http://google.com"
+url = "https://raw.githubusercontent.com/Ty-Greve/CMPUT404Lab1/main/test1.py"
 res = requests.get(url)
 print(res)
